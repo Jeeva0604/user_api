3abf284bed2580e74fbc466bfe2d1c1808edf403
Jeeva0604 <jeevajeeva06042002@gmail.com>
2024-03-02 16:42:07 +0530

    api_for_product

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const userRoutes = require("./routes/user_routes");
+const productRoutes = require("./routes/product_route");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use("/api", userRoutes);
+app.use("/api", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
